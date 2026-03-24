46293248dac4011809cf0e8e833f265b6bfdd7b0
Sahil karmakar <157059634+sahil43karmakar@users.noreply.github.com>
2026-03-24 22:47:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://interviewai-3.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
