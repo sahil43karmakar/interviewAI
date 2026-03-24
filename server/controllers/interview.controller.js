62f7f5b00c2c042ae119590d2271af68a131d5ea
Sahil karmakar <sahil43karmakar@gmail.com>
2026-03-24 22:18:29 +0530

    final fix remove pdfjs

--- server/controllers/interview.controller.js
+++ server/controllers/interview.controller.js
@@ -1,10 +1,12 @@
 import fs from "fs";
-import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const pdf = require("pdf-parse");
 
 import { askAi } from "../services/openRouter.service.js";
 import User from "../models/user.model.js";
 import Interview from "../models/interview.model.js";
-
 export const analyzeResume = async (req, res) => {
   try {
     if (!req.file) {
